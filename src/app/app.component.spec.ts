d7659c01cc8e61d36a099b012a651246a20b4e9b
Xyp9xGod <arilson.jlle@gmail.com>
2022-01-24 20:02:00 -0300

    Criação do componente e primeiros passos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'gerenciador-de-tarefas'`, () => {
+  it(`should have as title 'Gerenciador de Tarefas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gerenciador-de-tarefas');
+    expect(app.title).toEqual('Gerenciador de Tarefas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('gerenciador-de-tarefas app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Gerenciador de Tarefas app is running!');
   });
 });
